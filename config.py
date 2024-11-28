6420f6418e7a203efe13ce479c1812cfe2f7be48
emiliano <emipaz1975@hotmail.com>
2024-11-28 19:42:37 -0300

    carga bot, html, estilos

---
+++ config.py
@@ -0,0 +1,69 @@
+MEMORIA = 10
+DOCUMENTOS = 10
+MODELO = "gpt-4o-mini"
+# MODELO = "gpt-3.5-turbo"
+# MODELO = "gpt-4-turbo"
+
+BASE_1  = "bases/base_1"
+CURSO_1 = "Introduction to Generative AI"
+ALIAS_1 = "Gen AI Modulo 1"
+DESCRIPCION_1 = """This course is designed to enhance your skills by integrating AI chatbots as pair programmers in your development process. You’ll learn about how large language models (LLMs) work and how this general-purpose technology can be applied to common software development tasks to help boost productivity, creativity, and support you in your tasks as a developer. 
+
+By the end of this course, you will be able to:
+- Understand the differences between machine learning and traditional software development
+- Describe how large language models generate text
+- Prompt an LLM to assist in the tasks that make up the software developer role
+- Guide an LLM to complete a task in a specific way by writing detailed prompts and iterating to improve output
+- Leverage the depth of software development knowledge encoded in an LLM by prompting it to assume specific job roles or personas
+- Write code quickly using an LLM as a pair-coding partner
+- Analyze code for efficiency, security, and performance using an LLM
+
+This course assumes you have a background in software development, 
+but are new to using LLMs as part of your development process. 
+By knowing how machine learning systems work, and having an 
+understanding of how they can be applied in software development, 
+you’ll be able to use them more effectively to support 
+your growth and work as a developer or engineer.
+"""
+TEMARIO_1 = """
+- What is Generative AI?
+- AI and machine learning
+- Machine learning example
+- Supervised learning
+- Introduction to transformers
+- Key transformer concepts
+- Setting up your Jupyter environment
+- ejemplos de codigos en distintos lenguajes de programacion
+"""
+
+BASE_2  = "bases/base_2"
+CURSO_2 = "Pair-coding with an LLM"
+ALIAS_2 = "Gen AI Modulo 2"
+DESCRIPCION_2 = DESCRIPCION_1
+TEMARIO_2 = """
+- Transformers and code•3 minutos
+- Generating code with chatbots
+- Iterative prompting
+- Giving the LLM feedback
+- Assigning the LLM a role
+- Leveling up with multiple roles
+- Expert roles for specialized knowledge
+- LLM best practices
+- ejemplos de codigos en distintos lenguajes de programacion
+"""
+
+BASE_3 = "bases/base_3"
+CURSO_3 = "Leveraging an LLM for code analysis"
+ALIAS_3 = "Gen AI Modulo 3"
+DESCRIPCION_3 = DESCRIPCION_1
+TEMARIO_3 = """
+- Arrays
+- Linked lists
+- Self-paced exercise: Doubly-linked Lists 
+- Trees
+- Graphs
+- Graded lab: Graph algorithms
+- Hash tables and maps
+- ejemplos de codigos en distintos lenguajes de programacion
+"""
+
